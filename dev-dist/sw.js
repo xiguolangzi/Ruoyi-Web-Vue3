1b9acb25b22ff057705275488ddd1665d9da54aa
xiguolangzi <hubiao19910129@gmail.com>
2025-03-18 10:17:18 +0100

    收银台优化

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ec4a7e67'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hft8e05g5l"
+    "revision": "0.dep8ht8pqeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

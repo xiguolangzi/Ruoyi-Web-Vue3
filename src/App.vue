d59114edb698d7cb6e3e9a4705cb99fcbb5db5b4
xiguolangzi <hubiao19910129@gmail.com>
2025-04-11 09:34:02 +0200

    消息通知

--- src/App.vue
+++ src/App.vue
@@ -22,26 +22,26 @@ locale.value = lang.value
 
 /** pwa - 需要网页加载完成后，注册 service worker */
 window.addEventListener('load', () => {
-  // 1 能力检测浏览器是否支持 serviceWorker -> 注册 serviceWorker
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.register('/service-worker.js').then(registration => {
-  //     console.log('Service Worker 注册成功', registration);
-  //   }).catch(error => {
-  //     console.log('Service Worker 注册失败', error);
-  //   });
-  // }
-  // 2 pwa - 断网提示 
-  // if (!navigator.onLine) {
-  //   new Notification('网络已断开', {
-  //     body: '请检查网络连接! 由于断网导致您使用的是本地缓存数据! '
-  //   })
-  // }
-  // // 3 pwa - 连网提示 
-  // window.addEventListener('online', () => {
-  //   new Notification('网络已恢复', {
-  //     body: '网络已恢复, 您的链接是实时数据!'
-  //   })
-  // })
+  //1 能力检测浏览器是否支持 serviceWorker -> 注册 serviceWorker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker 注册成功', registration);
+    }).catch(error => {
+      console.log('Service Worker 注册失败', error);
+    });
+  }
+  //2 pwa - 断网提示 
+  if (!navigator.onLine) {
+    new Notification('网络已断开', {
+      body: '请检查网络连接! 由于断网导致您使用的是本地缓存数据! '
+    })
+  }
+  // 3 pwa - 连网提示 
+  window.addEventListener('online', () => {
+    new Notification('网络已恢复', {
+      body: '网络已恢复, 您的链接是实时数据!'
+    })
+  })
 
 },{passive:true})
 

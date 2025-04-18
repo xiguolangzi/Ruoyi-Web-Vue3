12f9cfdd4989bb658aa02a16029066fec7720322
xiguolangzi <hubiao19910129@gmail.com>
2025-04-18 04:14:30 +0200

    租户数据字段优化

--- src/views/system/tenant/index.vue
+++ src/views/system/tenant/index.vue
@@ -159,28 +159,38 @@
 
     <el-table class="table-container" v-loading="loading" :data="tenantList" @selection-change="handleSelectionChange" >
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="租户编码" align="center" prop="tenantId" />
-      <el-table-column label="税号" align="center" prop="taxNumber" />
-      <el-table-column label="租户名称" align="center" prop="tenantName" />
-      <el-table-column label="租户类型" align="center" prop="tenantType">
+      <el-table-column label="公司编码" align="center" prop="tenantCode" />
+      <el-table-column label="公司名称" align="center" prop="tenantName" />
+      <el-table-column label="公司类型" align="center" prop="tenantType" width="80">
         <template #default="scope">
           <dict-tag :options="sys_tenant_type" :value="scope.row.tenantType"/>
         </template>
       </el-table-column>
-      <el-table-column label="帐号状态" align="center" prop="status">
+      <el-table-column label="公司状态" align="center" prop="tenantStatus" width="80">
         <template #default="scope">
-          <dict-tag :options="sys_tenant_status" :value="scope.row.status"/>
+          <dict-tag :options="sys_tenant_status" :value="scope.row.tenantStatus"/>
         </template>
       </el-table-column>
-      <el-table-column label="手机号" align="center" prop="phoneNumber" />
-      <el-table-column label="邮编" align="center" prop="postalCode" />
-      <el-table-column label="地址" align="center" prop="address" />
-      <el-table-column label="邮箱" align="center" prop="email" />
-      <el-table-column label="备注" align="center" prop="remark" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column label="成本计算" align="center" prop="costMethod">
+        <template #default="scope">
+          <dict-tag :options="product_cost_method" :value="scope.row.costMethod"/>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="CIF/NIF" align="center" prop="invoiceTaxNumber"  show-overflow-tooltip/>
+      <el-table-column label="欧盟增值税号" align="center" prop="invoiceVatNumber"  show-overflow-tooltip/>
+      <el-table-column label="法定名称" align="center" prop="invoiceCompanyName" show-overflow-tooltip/>
+      <el-table-column label="手机号" align="center" prop="invoicePhoneNumber" show-overflow-tooltip/>
+      <el-table-column label="邮编" align="center" prop="invoicePostalCode" show-overflow-tooltip/>
+      <el-table-column label="地址国家" align="center" prop="invoiceAddressCountry" show-overflow-tooltip/>
+      <el-table-column label="地址省份" align="center" prop="invoiceAddressProvince" show-overflow-tooltip/>
+      <el-table-column label="地址明细" align="center" prop="invoiceAddressDetail" show-overflow-tooltip/>
+      <el-table-column label="邮箱" align="center" prop="email" show-overflow-tooltip/>
+      <el-table-column label="备注" align="center" prop="remark" show-overflow-tooltip/>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="130">
         <template #default="scope">
           <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['system:tenant:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:tenant:remove']">删除</el-button>
+          <el-button link type="danger" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['system:tenant:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -194,59 +204,112 @@
     />
 
 
-    
-
     <!-- 添加或修改租户信息对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
       <el-form ref="tenantRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="税号" prop="taxNumber">
-          <el-input v-model="form.taxNumber" placeholder="请输入税号" />
-        </el-form-item>
-        <el-form-item label="租户名称" prop="tenantName">
-          <el-input v-model="form.tenantName" placeholder="请输入租户名称" />
-        </el-form-item>
-        <el-form-item label="租户类型" prop="tenantType">
-          <el-select v-model="form.tenantType" placeholder="请选择租户类型">
-            <el-option
-              v-for="dict in sys_tenant_type"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="帐号状态" prop="status">
-          <el-select v-model="form.status" placeholder="请选择帐号状态">
-            <el-option
-              v-for="dict in sys_tenant_status"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="手机号" prop="phoneNumber">
-          <el-input v-model="form.phoneNumber" placeholder="请输入手机号" />
-        </el-form-item>
-        <el-form-item label="邮编" prop="postalCode">
-          <el-input v-model="form.postalCode" placeholder="请输入邮编" />
-        </el-form-item>
-        <el-form-item label="地址" prop="address">
-          <el-input v-model="form.address" placeholder="请输入地址" />
-        </el-form-item>
-        <el-form-item label="邮箱" prop="email">
-          <el-input v-model="form.email" placeholder="请输入邮箱" />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
+        <el-tabs class="tab-container" v-model="activeName" type="border-card" style="margin: 0 20px" >
+          <!-------   基础信息部分   --------->
+          <el-tab-pane  label="商品基础信息" name="first">
+            <el-form-item label="公司编码:" prop="tenantCode">
+              <el-input v-model="form.tenantCode" placeholder="请输入租户名称" type="textarea" maxlength="30" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="公司名称:" prop="tenantName">
+              <el-input v-model="form.tenantName" placeholder="请输入租户名称" type="textarea" maxlength="100" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="公司类型:" prop="tenantType">
+              <el-select v-model="form.tenantType" placeholder="请选择租户类型">
+                <el-option
+                  v-for="dict in sys_tenant_type"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="Number(dict.value)"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="帐号状态:" prop="tenantStatus">
+              <el-select v-model="form.tenantStatus" placeholder="请选择帐号状态">
+                <el-option
+                  v-for="dict in sys_tenant_status"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="Number(dict.value)"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="成本计算方式:" prop="costMethod">
+              <el-select v-model="form.costMethod" placeholder="请选择租户类型">
+                <el-option
+                  v-for="dict in product_cost_method"
+                  :key="dict.value"
+                  :label="dict.label"
+                  :value="Number(dict.value)"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="备注:" prop="remark">
+              <el-input v-model="form.remark" placeholder="请输入内容" type="textarea" maxlength="200" show-word-limit :rows="2"/>
+            </el-form-item>
+          </el-tab-pane>
+          <!-----------------   发票信息  ----------------->
+          <el-tab-pane label="发票信息" name="second">
+            <el-form-item label="CIF/NIF:" prop="invoiceTaxNumber">
+              <el-input v-model="form.invoiceTaxNumber" placeholder="请输入CIF企业/NIF个人" type="textarea" maxlength="20" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="欧盟增值税号:" prop="invoiceVatNumber">
+              <el-input v-model="form.invoiceVatNumber" placeholder="请输入欧盟增值税号" type="textarea" maxlength="20" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="法定名称:" prop="invoiceCompanyName">
+              <el-input v-model="form.invoiceCompanyName" placeholder="公司名称" type="textarea" maxlength="100" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="手机号:" prop="invoicePhoneNumber">
+              <el-input v-model="form.invoicePhoneNumber" placeholder="请输入手机号" />
+            </el-form-item>
+            <el-form-item label="公司邮箱:" prop="email">
+              <el-input v-model="form.email" placeholder="请输入邮箱" type="textarea" maxlength="100" show-word-limit :rows="1"/>
+            </el-form-item>
+            <el-form-item label="邮编:" prop="invoicePostalCode">
+              <el-input v-model="form.invoicePostalCode" placeholder="请输入邮编" />
+            </el-form-item>
+            <el-form-item label="地址-国家:" prop="invoiceAddressCountry">
+              <el-select v-model="form.invoiceAddressCountry" placeholder="请输入地址国家" filterable clearable style="width: 100%" @change="handleAddressCountryChange">
+                <el-option
+                  v-for="item in addressCountryList"
+                  :key="item.countryId"
+                  :label="`${item.countryCode} - ${item.nameEs} - ${item.nameZh}`"
+                  :value="item.nameEs"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="地址-省份:" prop="invoiceAddressProvince">
+              <el-select v-model="form.invoiceAddressProvince" placeholder="请输入地址省份" filterable clearable style="width: 100%" v-if="addressProvinceList.length > 0">
+                <el-option
+                  v-for="item in addressProvinceList"
+                  :key="item.provinceId"
+                  :label="`${item.provinceCode} - ${item.provinceNameEs} - ${item.provinceNameZh}`"
+                  :value="item.provinceNameEs"
+                >
+                </el-option>
+              </el-select>
+              <el-input v-else v-model="form.invoiceAddressProvince" placeholder="请输入地址省份" type="textarea" maxlength="50" show-word-limit :rows="1"/>
+            </el-form-item>
+            
+            <el-form-item label="地址-明细:" prop="invoiceAddressDetail">
+              <el-input v-model="form.invoiceAddressDetail" placeholder="请输入地址明细" type="textarea" maxlength="200" show-word-limit :rows="2"/>
+            </el-form-item>
+            
+          </el-tab-pane>
+        </el-tabs>
+
       </el-form>
+
       <template #footer>
         <div class="dialog-footer">
           <el-button type="primary" @click="submitForm">确 定</el-button>
           <el-button @click="cancel">取 消</el-button>
         </div>
       </template>
+
     </el-dialog>
   </div>
 </template>
@@ -254,13 +317,15 @@
 <script setup name="Tenant">
 import { listTenant, getTenant, delTenant, addTenant, updateTenant, addTenantDept, addTenantAdmin } from "@/api/system/tenant";
 import useUserStore from "@/store/modules/user";
-import { ElMessage, ElMessageBox } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { listAddressCountry } from "@/api/address/addressCountry";
+import { listAddressProvince } from "@/api/address/addressProvince";
 
 // 租户ID字段过滤使用
 const userStore = useUserStore();
 
 const { proxy } = getCurrentInstance();
-const { sys_tenant_status, sys_tenant_type } = proxy.useDict('sys_tenant_status', 'sys_tenant_type');
+const { sys_tenant_status, sys_tenant_type, product_cost_method } = proxy.useDict('sys_tenant_status', 'sys_tenant_type', 'product_cost_method');
 
 const tenantList = ref([]);
 const open = ref(false);
@@ -271,37 +336,106 @@ const single = ref(true);
 const multiple = ref(true);
 const total = ref(0);
 const title = ref("");
+// 标签菜单默认首页
+const activeName = ref("first");
+const addressCountryList = ref([]);
+const addressProvinceList = ref([]);
+
+
+/**
+ * 获取地址数据
+ */
+const getAddressCountryList = () => {
+  const param = {
+    pageNum: 1,
+    pageSize: 500
+  }
+  listAddressCountry(param).then(response => {
+    if(response.rows){
+      addressCountryList.value = response.rows;
+    }
+  }).catch(error => {
+    console.error("获取地址国家列表失败：", error);
+  });
+}
+
+/**
+ * 获取省份地址数据
+ */
+const getAddressProvinceList = () => {
+  const param = {
+    pageNum: 1,
+    pageSize: 500,
+    countryCode: 'ES'
+  }
+  listAddressProvince(param).then(response => {
+    if(response.rows){
+      addressProvinceList.value = response.rows;
+    }
+  }).catch(error => {
+    console.error("获取地址省份列表失败：", error);
+  })
+}
+
+const handleAddressCountryChange = () => {
+  form.value.invoiceAddressProvince = null;
+  form.value.invoiceAddressDetail = null;
+  if(form.value.invoiceAddressCountry){
+    if(form.value.invoiceAddressCountry == 'España'){
+      getAddressProvinceList();
+    } else{
+      addressProvinceList.value = [];
+    }
+  } else{
+    addressProvinceList.value = [];
+  }
+}
+
 
 const data = reactive({
   form: {},
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    taxNumber: null,
+    tenantCode: null,
     tenantName: null,
     tenantType: null,
-    status: null,
-    phoneNumber: null,
+    tenantStatus: null,
+    invoiceTaxNumber: null,
+    invoiceVatNumber: null,
+    invoicePhoneNumber: null,
     email: null,
   },
   rules: {
-    taxNumber: [
-      { required: true, message: "税号不能为空", trigger: "blur" }
+    tenantCode: [
+      { required: true, message: "公司编码不能为空", trigger: "blur" }
     ],
     tenantName: [
-      { required: true, message: "租户名称不能为空", trigger: "blur" }
+      { required: true, message: "公司名称不能为空", trigger: "blur" }
     ],
     tenantType: [
-      { required: true, message: "租户类型不能为空", trigger: "change" }
+      { required: true, message: "公司类型不能为空", trigger: "change" }
     ],
-    phoneNumber: [
-      { required: true, message: "手机号不能为空", trigger: "blur" }
+    invoiceTaxNumber: [
+      { required: true, message: "发票税号不能为空", trigger: "blur" }
     ],
-    postalCode: [
-      { required: true, message: "邮编不能为空", trigger: "blur" }
+    invoiceCompanyName: [
+      { required: true, message: "发票公司不能为空", trigger: "blur" }
     ],
-    address: [
-      { required: true, message: "地址不能为空", trigger: "blur" }
+    invoicePhoneNumber: [
+      { required: true, message: "发票手机号不能为空", trigger: "blur" }
+    ],
+    invoicePostalCode: [
+      { required: true, message: "发票邮编不能为空", trigger: "blur" }
+    ],
+    invoiceAddressCountry: [
+      { required: true, message: "发票地址国家不能为空", trigger: "blur" }
+    ],
+    invoiceAddressProvince: [
+      { required: true, message: "发票地址省份不能为空", trigger: "blur" }
+    ],
+    invoiceAddressDetail: [
+      { required: true, message: "发票地址明细不能为空", trigger: "blur" }
     ],
     email: [
       { required: true, message: "邮箱不能为空", trigger: "blur" }
@@ -333,21 +467,24 @@ function cancel() {
 function reset() {
   form.value = {
     tenantId: null,
-    taxNumber: null,
+    tenantCode: null,
     tenantName: null,
-    tenantType: null,
-    status: null,
-    phoneNumber: null,
-    postalCode: null,
-    address: null,
+    tenantType: 2,
+    tenantStatus: 0,
+    costMethod: 1,
+    invoiceTaxNumber: null,
+    invoiceCompanyName: null,
+    invoicePhoneNumber: null,
+    invoicePostalCode: null,
+    invoiceAddressCountry: null,
+    invoiceAddressProvince: null,
+    invoiceAddressDetail: null,
+    invoiceVatNumber: null,
     email: null,
-    createBy: null,
-    createTime: null,
-    updateBy: null,
-    updateTime: null,
     remark: null
   };
   proxy.resetForm("tenantRef");
+  activeName.value = "first";
 }
 
 /** 搜索按钮操作 */
@@ -374,6 +511,7 @@ function handleAdd() {
   reset();
   open.value = true;
   title.value = "添加租户信息";
+  getAddressCountryList()
 }
 
 /** 修改按钮操作 */
@@ -385,6 +523,7 @@ function handleUpdate(row) {
     open.value = true;
     title.value = "修改租户信息";
   });
+  getAddressCountryList()
 }
 
 /** 提交按钮 */
@@ -395,12 +534,14 @@ function submitForm() {
         updateTenant(form.value).then(response => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
+          activeName.value = "first";
           getList();
         });
       } else {
         addTenant(form.value).then(response => {
           proxy.$modal.msgSuccess("新增成功");
           open.value = false;
+          activeName.value = "first";
           getList();
         });
       }
